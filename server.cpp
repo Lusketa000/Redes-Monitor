bf5e26b5fbd053f2516c1354e6e7827532106afa
Lusketa <lucascamargo2004@gmail.com>
2025-08-26 18:31:43 -0300

    MEM funcional

--- server.cpp
+++ server.cpp
@@ -1,79 +1,115 @@
-// C++ program to show the example of server application in
-// socket programming
-#include <cstring>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>
-#include <thread>
-#include <tuple>
-#include <vector>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 
-void input(int clientSocket) {
-    cout << "funcao input" << endl;
-    char buffer[1024];
-    int bytesReceived;
-    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
-        buffer[bytesReceived] = '\0'; // Garante terminação
-        cout << "Message from client " << clientSocket << ": " << buffer << endl;
-        memset(buffer, 0, sizeof(buffer)); // Limpa o buffer
+void output(int clientSocket, pid_t pid, int tempo);
+void input(int clientSocket, pid_t pid);
+
+// Flag de controle global (output QUIT)
+atomic<bool> keepRunning(false);
+
+// Função para ler memória de um PID
+string getMemoryUsage(pid_t pid) {
+    ifstream statusFile("/proc/" + to_string(pid) + "/status");
+    if (!statusFile) return "Processo não encontrado\n";
+
+    string line;
+    long mem_kb = 0;
+    while (getline(statusFile, line)) {
+        if (line.rfind("VmRSS:", 0) == 0) { 
+            mem_kb = stol(line.substr(6));
+            break;
+        }
     }
-    // TODO: Verificar o comando e criar uma thread output de acordo
+    return "Memória usada: " + to_string(mem_kb) + " kB\n";
 }
 
-void output(int clientSocket, int tempo) {
-    while(1) {
-        // TODO: Vê na flag global se tem que parar
+// Thread de entrada (input) que lê comandos do cliente
+void input(int clientSocket, pid_t pid) {
+    char buffer[1024];
+    int bytesReceived;
+    thread outputThread;
 
-        // sending data
-        const char* message = "Hello, client!";
-        send(clientSocket, message, strlen(message), 0);
+    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytesReceived] = '\0'; // garante terminação
 
+        cout << "Comando do cliente " << clientSocket << ": " << buffer << endl;
+
+        string cmd;
+        int Tempo = 1;
+
+        istringstream iss(buffer);
+
+        iss >> cmd;
+        iss >> Tempo;
+
+        if (cmd == "MEM\n" || cmd == "MEM") {
+            if (!keepRunning) {
+                keepRunning = true;
+                outputThread = thread(output, clientSocket, pid, Tempo); // envia memória a cada X segundos
+                outputThread.detach();
+            }
+        } else if (cmd == "QUIT\n" || cmd == "QUIT") {
+            keepRunning = false;
+        } else {
+            string msg = "Comando desconhecido\n";
+            send(clientSocket, msg.c_str(), msg.size(), 0);
+        }
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    cout << "Cliente " << clientSocket << " desconectou.\n";
+    keepRunning = false;
+    close(clientSocket);
+}
+
+// Thread de saída (output) que envia info para o Client
+void output(int clientSocket, pid_t pid, int tempo) {
+    while (keepRunning) {
+        string mem = getMemoryUsage(pid);
+        send(clientSocket, mem.c_str(), mem.size(), tempo);
         this_thread::sleep_for(chrono::seconds(tempo));
     }
 }
 
-int main()
-{
-    // creating socket
+int main() {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // specifying the address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // binding socket.
-    bind(serverSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
-
-    // listening to the assigned socket
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
     listen(serverSocket, 5);
 
     vector<thread> threads;
+    pid_t pid = getpid();
 
-    while (1) {
-        // accepting connection request
+    cout << "Servidor aguardando conexão...\n";
+
+    while (true) {
         int clientSocket = accept(serverSocket, nullptr, nullptr);
-        //clientSockets.push_back(clientSocket);
+        cout << "Cliente conectado! Socket: " << clientSocket << endl;
 
-        cout << "conectou um cliente!!!!!!11" << endl;
-        
-        // sending data
-        const char* message = "Oi cliente";
-        send(clientSocket, message, strlen(message), 0);
+        const char* welcomeMsg = "Bem-vindo ao servidor!\n";
+        send(clientSocket, welcomeMsg, strlen(welcomeMsg), 0);
 
-        threads.emplace_back(
-            thread(input, clientSocket)
-        );
+        threads.emplace_back(thread(input, clientSocket, pid));
     }
-        
-    // TODO: Consertar pq senao nunca vai chegar aq
-    // closing the socket.
-    //close(serverSocket);
 
+    close(serverSocket);
     return 0;
 }

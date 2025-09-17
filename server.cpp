c0b8d00f3d964a1f9e5069f110ade8cdb282dd66
Felipe1885 <felipe.leonardi08@gmail.com>
2025-09-16 22:09:10 -0300

    Deadlock no exit do servidor arrumado

--- server.cpp
+++ server.cpp
@@ -330,6 +330,7 @@ int main(int argc, char *argv[]) {
 
     //Fecha todos os sockets
     for(auto const& [thr, sock] : clientThreads) {
+        shutdown(sock, SHUT_RDWR);
         close(sock);
     }
 
